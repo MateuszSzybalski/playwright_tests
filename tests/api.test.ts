c625b3f5ebcb8e0c6ac2e80057180d76a96f02b9
Mateusz Szybalski <mszybalski@wieltongroup.com>
2024-10-18 23:55:25 +0200

    add api tests

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -12,21 +12,21 @@ test.describe('API tests', () => {
     await apiContext.dispose();
   });
 
-  test('GET for posts', async () => {
+  test('GET - posts', async () => {
     const response = await apiContext.get('https://jsonplaceholder.typicode.com/posts');
     expect(response.status()).toBe(200);
     
-    const posts = await response.json();
-    expect(Array.isArray(posts)).toBe(true);
-    expect(posts.length).toBeGreaterThan(0);
+    const get = await response.json();
+    expect(Array.isArray(get)).toBe(true);
+    expect(get.length).toBeGreaterThan(0);
     
-    expect(posts[0]).toHaveProperty('userId');
-    expect(posts[0]).toHaveProperty('id');
-    expect(posts[0]).toHaveProperty('title');
-    expect(posts[0]).toHaveProperty('body');
+    expect(get[0]).toHaveProperty('userId');
+    expect(get[0]).toHaveProperty('id');
+    expect(get[0]).toHaveProperty('title');
+    expect(get[0]).toHaveProperty('body');
   });
 
-  test('POST for posts', async () => {
+  test('POST - posts', async () => {
     const newPost = {
         userId: 999,
         title: 'Tester Test',
@@ -43,37 +43,78 @@ test.describe('API tests', () => {
       expect(post).toHaveProperty('body', newPost.body);
     });
 
-  test('GET for comments', async () => {
+    test('PUT - posts', async () => {
+        const postId = 99;
+        const newPut = {
+          userId: 999,
+          title: 'Tester Test',
+          body: 'Test Test Test'
+        };
+    
+        const response = await apiContext.put(`https://jsonplaceholder.typicode.com/posts/${postId}`, {data: newPut});    
+        expect(response.status()).toBe(200);
+    
+        const put = await response.json();
+        expect(put).toHaveProperty('id', postId);
+        expect(put).toHaveProperty('userId', newPut.userId);
+        expect(put).toHaveProperty('title', newPut.title);
+        expect(put).toHaveProperty('body', newPut.body);
+      });
+
+  test('GET - comments', async () => {
     const response = await apiContext.get('https://jsonplaceholder.typicode.com/comments');
     expect(response.status()).toBe(200);
     
-    const comments = await response.json();
-    expect(Array.isArray(comments)).toBe(true);
-    expect(comments.length).toBeGreaterThan(0);
+    const get = await response.json();
+    expect(Array.isArray(get)).toBe(true);
+    expect(get.length).toBeGreaterThan(0);
     
-    expect(comments[0]).toHaveProperty('postId');
-    expect(comments[0]).toHaveProperty('id');
-    expect(comments[0]).toHaveProperty('name');
-    expect(comments[0]).toHaveProperty('email');
-    expect(comments[0]).toHaveProperty('body');
+    expect(get[0]).toHaveProperty('postId');
+    expect(get[0]).toHaveProperty('id');
+    expect(get[0]).toHaveProperty('name');
+    expect(get[0]).toHaveProperty('email');
+    expect(get[0]).toHaveProperty('body');
   });
 
-  test('POST for comments', async () => {
-    const newComment = {
+  test('POST - comments', async () => {
+    const newPost = {
       postId: 999,
       name: 'Tester',
       email: 'tester@test.com',
       body: 'Test'
     };
 
-    const response = await apiContext.post('https://jsonplaceholder.typicode.com/comments', {data: newComment});
+    const response = await apiContext.post('https://jsonplaceholder.typicode.com/comments', {data: newPost});
     expect(response.status()).toBe(201);
 
-    const comment = await response.json();
-    expect(comment).toHaveProperty('postId', newComment.postId);
-    expect(comment).toHaveProperty('name', newComment.name);
-    expect(comment).toHaveProperty('email', newComment.email);
-    expect(comment).toHaveProperty('body', newComment.body);
-    expect(comment).toHaveProperty('id');
+    const post = await response.json();
+    expect(post).toHaveProperty('postId', newPost.postId);
+    expect(post).toHaveProperty('name', newPost.name);
+    expect(post).toHaveProperty('email', newPost.email);
+    expect(post).toHaveProperty('body', newPost.body);
+    expect(post).toHaveProperty('id');
+  });
+
+  
+  test('PUT - comments', async () => {
+    const commentId = 99;
+    const newPut = {
+      postId: 999,
+      userId: 999,
+      name: 'Tester Test',
+      email: 'tester@example.com',
+      body: 'Test Test Test'
+    };
+
+    const response = await apiContext.put(`https://jsonplaceholder.typicode.com/comments/${commentId}`, {data: newPut});
+    expect(response.status()).toBe(200);
+
+    const put = await response.json();
+    expect(put).toHaveProperty('id', commentId);
+    expect(put).toHaveProperty('postId', newPut.postId);
+    expect(put).toHaveProperty('userId', newPut.userId);
+    expect(put).toHaveProperty('name', newPut.name);
+    expect(put).toHaveProperty('email', newPut.email);
+    expect(put).toHaveProperty('body', newPut.body);
   });
 });
