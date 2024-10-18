a76fe5bf44c90f59cf338268983085e72bdba804
Mateusz Szybalski <mszybalski@wieltongroup.com>
2024-10-19 00:11:01 +0200

    update tests

--- tests/ui.test.ts
+++ tests/ui.test.ts
@@ -11,6 +11,14 @@ test.describe("UI tests", () => {
   let cartPage: CartPage;
   let checkoutPage: CheckoutPage;
 
+  const shoppingCartBadge: string = '[data-test="shopping-cart-badge"]';
+  const itemQuantity: string = '[data-test="item-quantity"]';
+  const subTotalLabel: string = '[data-test="subtotal-label"]';
+  const title: string = '[data-test="title"]';
+  const completeHeader: string = '[data-test="complete-header"]';
+  const inventoryItem: string = '[data-test="inventory-item"]';
+
+
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
     productsPage = new ProductsPage(page);
@@ -30,13 +38,13 @@ test.describe("UI tests", () => {
     const itemName = await productsPage.getItemName();
     const getItemPrice = await productsPage.getItemPrice();
     await productsPage.addItemIntoBasket();
-    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText("1");
+    await expect(page.locator(shoppingCartBadge)).toHaveText("1");
     await productsPage.clickShoppinghCart();
     const itemNameInCart = await cartPage.getItemNameInCart();
     const getItemPriceInCart = await cartPage.getItemPriceInCart();
     expect(itemName).toBe(itemNameInCart);
     expect(getItemPrice).toBe(getItemPriceInCart);
-    await expect(page.locator('[data-test="item-quantity"]')).toHaveText("1");
+    await expect(page.locator(itemQuantity)).toHaveText("1");
     await cartPage.clickCheckoutButton();
     await checkoutPage.fillTheForm("Test", "Tester", "97-300");
     await checkoutPage.clickContinueButton();
@@ -44,20 +52,20 @@ test.describe("UI tests", () => {
     const getItemPriceInCheckout = await checkoutPage.getItemPriceInCheckout();
     expect(itemNameInCart).toBe(itemNameInCheckout);
     expect(getItemPriceInCart).toBe(getItemPriceInCheckout);
-    await expect(page.locator("[data-test='subtotal-label']")).toHaveText("Item total: " + getItemPriceInCheckout);
+    await expect(page.locator(subTotalLabel)).toHaveText("Item total: " + getItemPriceInCheckout);
     await checkoutPage.clickFinishButton();
-    await expect(page.locator('[data-test="title"]')).toHaveText("Checkout: Complete!");
-    await expect(page.locator('[data-test="complete-header"]')).toHaveText("Thank you for your order!");
+    await expect(page.locator(title)).toHaveText("Checkout: Complete!");
+    await expect(page.locator(completeHeader)).toHaveText("Thank you for your order!");
   });
 
   test("Remove item from the basket", async ({ page }) => {
     await productsPage.selectItemByName("Sauce Labs Backpack");
     await productsPage.addItemIntoBasket();
     await productsPage.clickShoppinghCart();
-    await expect(page.locator('[data-test="inventory-item"]')).toBeVisible();
-    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText("1");
+    await expect(page.locator(inventoryItem)).toBeVisible();
+    await expect(page.locator(shoppingCartBadge)).toHaveText("1");
     await cartPage.clickRemoveButton();
-    await expect(page.locator('[data-test="inventory-item"]')).not.toBeVisible();
-    await expect(page.locator('[data-test="shopping-cart-badge"]')).not.toBeVisible();
+    await expect(page.locator(inventoryItem)).not.toBeVisible();
+    await expect(page.locator(shoppingCartBadge)).not.toBeVisible();
   });
 });
